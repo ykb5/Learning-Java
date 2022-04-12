4ef00d783c550f646f2c34d325401128ad53354c
Yogesh <91362274+ykb5@users.noreply.github.com>
2022-04-12 08:32:00 +0530

    First Commit.

---
+++ Learning-Java/src/Unit_01/P1_FirstJavaProgramInJava.java
@@ -0,0 +1,13 @@
+package Unit_01;
+
+public class P1_FirstJavaProgramInJava {
+
+    public static void main(String[] args) {
+
+        System.out.println("HELLO WORLD!");
+
+        System.out.println("This Is My First Java Program");
+
+    }
+
+}
